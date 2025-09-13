821b42c724ab481523a621956d4c9068dcc82113
Anurudha Sarkar <132932624+anurudha07@users.noreply.github.com>
2025-09-13 10:21:26 +0530

    Update Experience.js

--- frontend/src/pages/Experience.js
+++ frontend/src/pages/Experience.js
@@ -119,9 +119,9 @@ const Experience = () => {
       duration: "July 2024 – Aug 2025",
       logo: dbsLogo,
       bullets: [
-        "Built REST APIs with Node.js, improving data retrieval efficiency by 40%.",
+        "Built REST APIs on Node, improving data retrieval efficiency by 40%.",
         "Implemented LMS features like enrollment, assessments, and payments via Stripe.",
-        "Streamlined frontend with Next.js & ensuring typesafety for production readiness with TypeScript.",
+        "Streamlined web app with Next.js & ensuring type safety for production readiness with TypeScript.",
       ],
     },
   ];
